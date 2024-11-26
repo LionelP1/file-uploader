1f20617b9b88e1af7df5937955c46361756c9e54
Lionel <lpei1900@gmail.com>
2024-11-26 12:30:51 -0500

    Implemented the session and changed the schema

---
+++ config/session.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const expressSession = require('express-session');
+const PrismaSessionStore = require('@quixo3/prisma-session-store').PrismaSessionStore;
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+module.exports = expressSession({
+  store: new PrismaSessionStore(prisma, {
+    checkPeriod: 2 * 60 * 1000,
+  }),
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: false,
+  resave: false,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+});
