a987e7f2bce488d9c9a1c33b811ebf05ef450652
Lionel <lpei1900@gmail.com>
2024-11-28 10:39:35 -0500

    Added the create folder controller

--- controllers/folderControllers.js
+++ controllers/folderControllers.js
@@ -1,2 +1,16 @@
 const prisma = require("../prisma/queries");
 
+exports.createFolder = async (req, res) => {
+  try {
+    const { folderName } = req.body;
+    const { userId } = req.user;
+    const parentFolderId = req.params.parentFolderId ? parseInt(req.params.parentFolderId) : null;
+    
+    const newFolder = await prismaQueries.createFolder(userId, folderName, parentFolderId);
+
+    return res.status(201).json(newFolder);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'An error occurred while creating the folder.' });
+  }
+};
