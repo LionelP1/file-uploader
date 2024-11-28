ab5dcd2b249c904166bcc05a57f57bfdeca8ebc6
Lionel <lpei1900@gmail.com>
2024-11-28 09:27:18 -0500

    Implemented many queries for folders and files

--- prisma/queries.js
+++ prisma/queries.js
@@ -28,6 +28,71 @@ const prismaQueries = {
       },
     });
   },
+
+  createFolder: async (userId, folderName, parentId = null) => {
+    const existingFolder = await prisma.folder.findFirst({
+      where: {
+        userId,
+        folderName,
+        parentId,
+      },
+    });
+
+    if (existingFolder) {
+      throw new Error("A folder with this name already exists in the specified location.");
+    }
+
+    return await prisma.folder.create({
+      data: {
+        userId,
+        folderName,
+        parentId,
+      },
+    });
+  },
+
+
+  deleteFolder: async (folderId) => {
+    //How to handle the files and other 
+    //folders within that folder
+  },
+
+  getFolders: async (userId, parentId = null) => {
+    return await prisma.folder.findMany({
+      where: {
+        userId,
+        parentId,
+      },
+      include: {
+        children: true,
+      },
+    });
+  },
+
+  getSubFolders: async (userId, parentFolderId = null) => {
+    return await prisma.folder.findMany({
+      where: {
+        userId: userId,
+        parentId: parentFolderId, // null for root-level folders
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
+    });
+  },
+
+  getFilesInFolder: async (userId, folderId = null) => {
+    return await prisma.file.findMany({
+      where: {
+        userId: userId,
+        folderId: folderId, // null for root-level files
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
+    });
+  },
+
 };
 
 module.exports = prismaQueries;
