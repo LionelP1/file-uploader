e04b54f0d406c001a824bd24f7fe41b82f412282
Lionel <lpei1900@gmail.com>
2024-11-29 10:43:29 -0500

    Created the folder table view to display files and folders

--- routes/renderRoutes.js
+++ routes/renderRoutes.js
@@ -3,13 +3,12 @@ const renderController = require('../controllers/renderControllers');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get('/', renderController.getWelcomePage);
+router.get('/', authMiddleware.redirectIfAuthenticated, renderController.getWelcomePage);
 
 router.get('/login', authMiddleware.redirectIfAuthenticated, renderController.getLoginPage);
 
 router.get('/signup', authMiddleware.redirectIfAuthenticated, renderController.getSignupPage);
 
-// router.get('/homepage', authMiddleware.isAuthenticated, renderController.getHomePage);
 router.get('/homepage/:folderId?', authMiddleware.isAuthenticated, renderController.getHomePage);
 router.get('/create-folder/:folderId?', authMiddleware.isAuthenticated, renderController.getCreateFolderForm);
 module.exports = router;
