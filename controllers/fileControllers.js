4b7defa7d022b4a619c8db4ff28d05d6eef3f89a
Lionel <lpei1900@gmail.com>
2024-12-04 13:22:17 -0500

    Added file and folder deletion controllers

--- controllers/fileControllers.js
+++ controllers/fileControllers.js
@@ -23,3 +23,29 @@ exports.uploadFile = async (req, res, next) => {
     res.status(500).json({ error: 'An error occurred during the file upload' });
   }
 };
+
+exports.deleteFile = async (req, res) => {
+  const fileId = parseInt(req.params.fileId);
+  const userId = req.user.id;
+
+  try {
+    const deleted = await queries.deleteFile(fileId, userId);
+
+    if (!deleted.count) {
+      return res.status(404).json({ error: 'File not found or not authorized to delete.' });
+    }
+
+    const file = await queries.getFileById(fileId, userId);
+
+    const folderId = file.folderId;
+
+    if (parentFolderId) {
+      res.redirect(`/homepage/${folderId}`);
+    } else {
+      res.redirect('/homepage');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while deleting the file.' });
+  }
+};
--- controllers/folderControllers.js
+++ controllers/folderControllers.js
@@ -22,3 +22,31 @@ exports.createFolder = async (req, res) => {
     return res.status(500).json({ error: 'An error occurred while creating the folder.' });
   }
 };
+
+exports.deleteFolder = async (req, res) => {
+  const folderId = parseInt(req.params.folderId, 10);
+  const userId = req.user.id;
+
+  try {
+    const deleted = await queries.deleteFolder(folderId, userId);
+
+    if (!deleted.count) {
+      return res.status(404).json({ error: 'Folder not found or not authorized to delete.' });
+    }
+
+    const folder = await queries.getFolderById(folderId, userId);
+
+    const parentFolderId = folder.parentId;
+
+    if (parentFolderId) {
+      res.redirect(`/homepage/${parentFolderId}`);
+    } else {
+      res.redirect('/homepage');
+    }
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while deleting the folder.' });
+  }
+};
+
